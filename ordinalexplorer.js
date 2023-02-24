5c4be5046aafd85a45a0d4893a6ea6573727bbf2
solarzone898 <93065877+solarzone898@users.noreply.github.com>
2023-02-24 21:13:31 +1300

    Update ordinalexplorer.js

--- ordinalexplorer.js
+++ ordinalexplorer.js
@@ -1,12 +1,34 @@
 function FS(a,n){
-  if(a=='')return''
+  if(a==''){return''}
   if(a.slice(-2)=='[]'){return a.slice(0,-2)}
-  x=a.lastIndexOf('[]')+1;
-  y=x;
-  p=-2;
-  while(p!=0){y-=1;p+=(a[y]=='[')?1:-1}
-  o=a.slice(0,y)+(a.slice(y,x-1)+']').repeat(n);
-  l=o.split('[').length-o.split(']').length;
+  let x=a.lastIndexOf('[]')+1;
+  let i=x;
+  let p=-2;
+  while(p!=0){i-=1;p+=(a[i]=='[')?1:-1}
+  let o=a.slice(0,i)+(a.slice(i,x-1)+']').repeat(n);
+  let l=o.split('[').length-o.split(']').length;
   o+=']'.repeat(l);
   return o;
+}
+function fancy(a){
+  if(/^(\[])*$/.test(a)){return (a.length/2).toString()}
+  else{
+  let x=a[0];
+  let i=1;
+  let p=2;
+  while(p!=0){i+=1;p+=(a[i]=='[')?1:-1}
+  let e=a.slice(1,i);
+  let k=a.slice(0,i+1);
+  let c=a.slice(i+1);
+  i=0;
+  while(a.slice(i,i+k.length)==k){i+=k.length;}
+  i=i/k.length;
+  c=c.slice(k.length*(i-1));
+  let s='';
+  if(e=='[]'){s='&omega;';}
+  else{s=`&omega;<sup>${fancy(e)}</sup>`;}
+  if(i>1){s+=`${i}`;}
+  if(c!=''){s+=`+${fancy(c)}`};
+  return s;
+  }
 }
